5e3c363f797250231aafd0a854f3423492fd4739
islamhack01 <iaubakirov503@gmail.com>
2023-07-31 11:36:08 +0600

    .

---
+++ script.js
@@ -0,0 +1,128 @@
+//! loops
+
+//? for / for in / for of
+//? i++ /increment
+//? i-- /decrement
+
+//! i++ = i + 1
+//! i-- = i - 1
+
+// i++
+// ++i
+
+// let i = 0;
+
+// console.log(i++); //0
+// console.log(i++); //1
+// console.log(i++); //2 + 1 = 3 + 1 = 4
+// console.log(++i);
+// console.log(i++); //4 + 1
+// console.log(i);
+// console.log(i);
+// console.log(i);
+// console.log(i);
+
+// console.log(i++, "0");
+// console.log(++i, "2");
+// console.log(i++, "2");
+// console.log(++i, "4");
+// console.log(i++, "4");
+
+// console.log(++i);
+// console.log(++i);
+// console.log(++i);
+// console.log(++i);
+
+// console.log(i++, "0");
+// console.log(i++, "1");
+// console.log(++i, "3");
+// console.log(i++, "3");
+// console.log(i);
+
+// console.log(i);
+
+// for (let i = 0; i < 50; i++) {
+
+// }
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i of arr) {
+//   console.log(i);
+// }
+
+// for (let i in arr) {
+//   console.log(i);
+// }
+
+// console.log(arr[0] * 2);
+// console.log(arr[1] * 2);
+// console.log(arr[2] * 2);
+// console.log(arr[3] * 2);
+// console.log(arr[4] * 2);
+
+// for (let i = 1; i < 51; i++) {
+//   console.log(i);
+// }
+
+// 10;
+// 9;
+// 8;
+// 7;
+// 6;
+// 5;
+// 4;
+// 3;
+// 2;
+// 1;
+
+// let arr = ["Paul", "John", "George", "Ringo", "Yoko"];
+
+// for (let i of arr) {
+//   if (i.length > 4) {
+//     console.log(i);
+//   }
+// }
+
+// Paul
+// John
+// George
+// Ringo
+// Yoko
+
+// H
+// l
+// o
+
+// let arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+// выведите поочерёдно в консоль все элементы,
+// которые меньше 5, используя цикл for of
+
+// for (let i of arr) {
+//   if (i < 5) {
+//     console.log(i);
+//   }
+// }
+
+// Создайте строку str. Выведите в консоль только те символы,
+// у которых четный индексНапример:
+
+// let str = "Hello";
+
+// for (let i in str) {
+//   if (i % 2 === 0) {
+//     console.log(str[i]);
+//   }
+// }
+
+// Вывод:
+// Hl
+// o
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
